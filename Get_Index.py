d1c94159bafa0c9b594fdbefdfd89f03eb4e2570
ucfilho <42356381+ucfilho@users.noreply.github.com>
2019-11-26 14:58:07 -0300

    Create Get_Index.py

---
+++ Get_Index.py
@@ -0,0 +1,9 @@
+def Di_rel(X,DIo):
+  soma=0
+  for j in range(ncol):
+    for i in range(nrow):
+        Xj=np.mean(X[:,j])
+        soma=soma+(X[i,j]-Xj)**2
+  DI=(soma/nrow)**0.5
+  DIr=DI/DIo
+  return DIr
